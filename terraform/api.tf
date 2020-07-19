1ab1f596690b39a695bb24b16d36216eb4f328f4
Paul Galow <paulgalow@users.noreply.github.com>
2020-07-19 20:52:35 +0200

    Update syntax to support Terraform v0.12

--- terraform/api.tf
+++ terraform/api.tf
@@ -17,8 +17,8 @@ resource "aws_dynamodb_table" "worker" {
   }
 
   tags = {
-    Project = "${var.app_name}"
-    Stage   = "${var.stage}"
+    Project = var.app_name
+    Stage   = var.stage
   }
 }
 
@@ -31,12 +31,12 @@ resource "aws_appsync_graphql_api" "main" {
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
 
   user_pool_config {
-    user_pool_id   = "${aws_cognito_user_pool.main.id}"
-    aws_region     = "${var.region}"
+    user_pool_id   = aws_cognito_user_pool.main.id
+    aws_region     = var.region
     default_action = "ALLOW"
   }
 
-  schema = "${file("../schema.graphql")}"
+  schema = file("../schema.graphql")
 }
 
 ################
@@ -45,23 +45,23 @@ resource "aws_appsync_graphql_api" "main" {
 
 resource "aws_appsync_datasource" "worker" {
   name             = "WorkerTable"
-  api_id           = "${aws_appsync_graphql_api.main.id}"
-  service_role_arn = "${aws_iam_role.appsync_dynamo_datasource.arn}"
+  api_id           = aws_appsync_graphql_api.main.id
+  service_role_arn = aws_iam_role.appsync_dynamo_datasource.arn
   type             = "AMAZON_DYNAMODB"
 
   dynamodb_config {
-    table_name = "${aws_dynamodb_table.worker.name}"
+    table_name = aws_dynamodb_table.worker.name
   }
 }
 
 resource "aws_appsync_datasource" "notifier" {
   name             = "NotifierFunction"
-  api_id           = "${aws_appsync_graphql_api.main.id}"
-  service_role_arn = "${aws_iam_role.appsync_notifier_lambda_datasource.arn}"
+  api_id           = aws_appsync_graphql_api.main.id
+  service_role_arn = aws_iam_role.appsync_notifier_lambda_datasource.arn
   type             = "AWS_LAMBDA"
 
   lambda_config {
-    function_arn = "${var.notifier_fn_arn}"
+    function_arn = var.notifier_fn_arn
   }
 }
 
@@ -70,48 +70,48 @@ resource "aws_appsync_datasource" "notifier" {
 #############
 
 resource "aws_appsync_resolver" "listWorkers" {
-  api_id            = "${aws_appsync_graphql_api.main.id}"
+  api_id            = aws_appsync_graphql_api.main.id
   field             = "listWorkers"
   type              = "Query"
-  data_source       = "${aws_appsync_datasource.worker.name}"
-  request_template  = "${file("../resolvers/Query.listWorkers.req.vtl")}"
-  response_template = "${file("../resolvers/Query.listWorkers.res.vtl")}"
+  data_source       = aws_appsync_datasource.worker.name
+  request_template  = file("../resolvers/Query.listWorkers.req.vtl")
+  response_template = file("../resolvers/Query.listWorkers.res.vtl")
 }
 
 resource "aws_appsync_resolver" "createWorker" {
-  api_id            = "${aws_appsync_graphql_api.main.id}"
+  api_id            = aws_appsync_graphql_api.main.id
   field             = "createWorker"
   type              = "Mutation"
-  data_source       = "${aws_appsync_datasource.worker.name}"
-  request_template  = "${file("../resolvers/Mutation.createWorker.req.vtl")}"
-  response_template = "${file("../resolvers/Mutation.createWorker.res.vtl")}"
+  data_source       = aws_appsync_datasource.worker.name
+  request_template  = file("../resolvers/Mutation.createWorker.req.vtl")
+  response_template = file("../resolvers/Mutation.createWorker.res.vtl")
 }
 
 resource "aws_appsync_resolver" "updateWorker" {
-  api_id            = "${aws_appsync_graphql_api.main.id}"
+  api_id            = aws_appsync_graphql_api.main.id
   field             = "updateWorker"
   type              = "Mutation"
-  data_source       = "${aws_appsync_datasource.worker.name}"
-  request_template  = "${file("../resolvers/Mutation.updateWorker.req.vtl")}"
-  response_template = "${file("../resolvers/Mutation.updateWorker.res.vtl")}"
+  data_source       = aws_appsync_datasource.worker.name
+  request_template  = file("../resolvers/Mutation.updateWorker.req.vtl")
+  response_template = file("../resolvers/Mutation.updateWorker.res.vtl")
 }
 
 resource "aws_appsync_resolver" "deleteWorker" {
-  api_id            = "${aws_appsync_graphql_api.main.id}"
+  api_id            = aws_appsync_graphql_api.main.id
   field             = "deleteWorker"
   type              = "Mutation"
-  data_source       = "${aws_appsync_datasource.worker.name}"
-  request_template  = "${file("../resolvers/Mutation.deleteWorker.req.vtl")}"
-  response_template = "${file("../resolvers/Mutation.deleteWorker.res.vtl")}"
+  data_source       = aws_appsync_datasource.worker.name
+  request_template  = file("../resolvers/Mutation.deleteWorker.req.vtl")
+  response_template = file("../resolvers/Mutation.deleteWorker.res.vtl")
 }
 
 resource "aws_appsync_resolver" "notifyWorker" {
-  api_id            = "${aws_appsync_graphql_api.main.id}"
+  api_id            = aws_appsync_graphql_api.main.id
   field             = "notifyWorker"
   type              = "Mutation"
-  data_source       = "${aws_appsync_datasource.notifier.name}"
-  request_template  = "${file("../resolvers/Mutation.notifyWorker.req.vtl")}"
-  response_template = "${file("../resolvers/Mutation.notifyWorker.res.vtl")}"
+  data_source       = aws_appsync_datasource.notifier.name
+  request_template  = file("../resolvers/Mutation.notifyWorker.req.vtl")
+  response_template = file("../resolvers/Mutation.notifyWorker.res.vtl")
 }
 
 #######
@@ -136,15 +136,15 @@ resource "aws_iam_role" "appsync_dynamo_datasource" {
 }
 EOF
 
-  tags {
-    Project = "${var.app_name}"
-    Stage   = "${var.stage}"
+  tags = {
+    Project = var.app_name
+    Stage   = var.stage
   }
 }
 
 resource "aws_iam_role_policy" "appsync_dynamo_datasource" {
   name = "${var.namespace}-dynamo-datasource"
-  role = "${aws_iam_role.appsync_dynamo_datasource.id}"
+  role = aws_iam_role.appsync_dynamo_datasource.id
 
   policy = <<EOF
 {
@@ -190,15 +190,15 @@ resource "aws_iam_role" "appsync_notifier_lambda_datasource" {
 }
 EOF
 
-  tags {
-    Project = "${var.app_name}"
-    Stage   = "${var.stage}"
+  tags = {
+    Project = var.app_name
+    Stage   = var.stage
   }
 }
 
 resource "aws_iam_role_policy" "appsync_notifier_lambda_datasource" {
   name = "${var.namespace}-notifier-invocation"
-  role = "${aws_iam_role.appsync_notifier_lambda_datasource.id}"
+  role = aws_iam_role.appsync_notifier_lambda_datasource.id
 
   policy = <<EOF
 {
